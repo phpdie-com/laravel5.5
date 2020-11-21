7a6ec34ca767540b0f64f6d98d4c5856ee1dca46
phpdie-com <458638670@qq.com>
2020-11-21 12:11:05 +0800

    测试使用命令

---
+++ app/Console/Commands/CurlPost.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CurlPost extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'curl:post {url} {--data=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send post request';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //使用方法 命令行输入 php artisan curl:post www.baidu.com --data="age=3&name=zhangsan"
+        $url = $this->argument('url');
+        $data = $this->option('data');
+        echo '请求url:'. $url.PHP_EOL.'请求参数:'.$data.PHP_EOL;
+    }
+}
