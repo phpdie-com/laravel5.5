5b18cbea8d0308e347d1aca3c5fe5267b292710a
phpdie-com <458638670@qq.com>
2021-01-29 22:45:21 +0800

    laraveladmin

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ArticleController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Article';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article());
+        $grid->column('id','id');
+        $grid->column('title','标题');
+        $grid->column('cateid','分类');
+        $grid->column('content','内容');
+        $grid->column('create_uid','创建人');
+        $grid->column('status','启用状态');
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+
+
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article());
+        $form->select('title', '分类')->required();
+        $form->text('title', '标题')->required();
+        $form->textarea('content','内容')->required();
+        $form->switch('status', '启用状态');
+        // 两个时间显示
+        $form->display('created_at', '创建时间');
+        $form->display('updated_at', '修改时间');
+
+        return $form;
+    }
+}
